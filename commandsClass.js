30406e47ea08c7f804e40d88404eeb2bfdac93aa
cojored <66964845+cojored@users.noreply.github.com>
2021-07-05 20:24:40 -0400

    Update Command Handling
    Add Kick and Ban Actions also mention args to commands

--- commandsClass.js
+++ commandsClass.js
@@ -2,17 +2,43 @@ const commandData = {}
 
 function format(reply, message) {
     repl = reply.toString().replace("{firstMention}", message.mentions.members.first())
-    repl1 = repl.toString().replace("{firstMention.tag}", message.mentions.members.first().user.tag)
-    repl2 = repl.toString().replace("{firstMention.tag}", message.mentions.members.first().username)
+    repl1 = repl.toString().replace("{firstMention.tag}", message.mentions.users.first().tag)
+    repl2 = repl1.toString().replace("{firstMention.username}", message.mentions.users.first().username)
     return repl2
+}
+
+function kick(cmd, message) {
+    switch(cmd.action.kick.toString()){
+        case "firstMention":
+            message.mentions.members.first().kick()
+            if(cmd.reply){
+                message.channel.send(format(cmd.reply, message))
+            }
+            break;
+        default:
+            throw "Who should be kicked\n\nOptions:\nfirstMention"
+    }
+}
+
+function ban(cmd, message){
+    switch(cmd.action.ban.toString()){
+        case "firstMention":
+            message.mentions.members.first().ban()
+            if(cmd.reply){
+                message.channel.send(format(cmd.reply, message))
+            }
+        break;
+        default:
+            throw "Who should be banned\n\nOptions\nfirstMention"
+    }
 }
 
 class CommandsClass {
     create(cmdname, data){
-        if(data.actions){
-          commandData[cmdname] = {reply: data.reply, action: data.action}
+        if(data.action){
+          commandData[cmdname] = {reply: data.reply, action: data['action']}
         }else{
-        commandData[cmdname] = {reply: data.reply}
+            commandData[cmdname] = {reply: data.reply}
         }
     }
     exists(cmdName){
@@ -22,32 +48,22 @@ class CommandsClass {
         return commandData[cmdName]
     }
     execute(cmdName, message){
-        cmd = commandData[cmdName]
+       let cmd = commandData[cmdName]
         if(cmd.action){
             switch(Object.keys(cmd.action)[0].toString()){
                 case "kick":
-                    switch(cmd.action.kick.toString()){
-                        case "firstMention":
-                            message.mentions.members.first().kick()
-                            if(cmd.reply){
-                                message.channel.send(format(cmd.reply, message))
-                            }
-                        default:
-                            throw "Who should be kicked\n\nOptions:\nfirstMention"
-                    }
+                    kick(cmd, message)
+                    break;
                 case "ban":
-                    switch(cmd.action.ban.toString()){
-                        case "firstMention":
-                            message.mentions.members.first().ban()
-                            if(cmd.reply){
-                                message.channel.send(format(cmd.reply, message))
-                            }
-                        default:
-                            throw "Who should be banned\n\nOptions\nfirstMention"
-                    }
+                    ban(cmd, message)
+                    break;
                 default:
                     throw "What action should happen\n\nOptions:\nkick\nban"
             }
+        }else{
+            message.channel.send(format(cmd.reply, message))
         }
     }
 }
+
+module.exports = CommandsClass
