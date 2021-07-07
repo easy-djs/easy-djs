21c687a649b8ae53c91c3e8557b1e92650fdeb0e
cojored <66964845+cojored@users.noreply.github.com>
2021-07-06 20:58:55 -0400

    More Bug Fixes

--- modules/formatClass.js
+++ modules/formatClass.js
@@ -18,23 +18,36 @@ class formatClass {
 		) {
 			return {title: cmd.noMention || "You did not mention a member"};
 		} else if (options.some((el) => JSON.stringify(reply).includes(el))) {
-			return JSON.parse(JSON.stringify(reply), (k, v) => {
-				if (typeof v === "string") {
-					return replace(v, {
-						firstUserMention: message.mentions.members.first(),
-						"firstUserMention.tag": message.mentions.users.first().tag,
-						"firstUserMention.username":
-							message.mentions.users.first().username,
-						"firstUserMention.avatar": message.mentions.users
-							.first()
-							.avatarURL(),
-						author: message.author,
-						"author.tag": message.author.tag,
-						"author.username": message.author.username,
-						"author.avatar": message.author.avatarURL(),
-					});
-				} else return v;
-			});
+			if (!message.mentions.members.first()) {
+				return JSON.parse(JSON.stringify(reply), (k, v) => {
+					if (typeof v === "string") {
+						return replace(v, {
+							author: message.author,
+							"author.tag": message.author.tag,
+							"author.username": message.author.username,
+							"author.avatar": message.author.avatarURL(),
+						});
+					} else return v;
+				});
+			} else {
+				return JSON.parse(JSON.stringify(reply), (k, v) => {
+					if (typeof v === "string") {
+						return replace(v, {
+							firstUserMention: message.mentions.members.first(),
+							"firstUserMention.tag": message.mentions.users.first().tag,
+							"firstUserMention.username":
+								message.mentions.users.first().username,
+							"firstUserMention.avatar": message.mentions.users
+								.first()
+								.avatarURL(),
+							author: message.author,
+							"author.tag": message.author.tag,
+							"author.username": message.author.username,
+							"author.avatar": message.author.avatarURL(),
+						});
+					} else return v;
+				});
+			}
 		} else {
 			return reply;
 		}
@@ -48,16 +61,25 @@ class formatClass {
 		) {
 			return cmd.noMention || "You did not mention a member";
 		} else if (options.some((el) => reply.includes(el))) {
-			return replace(reply, {
-				firstUserMention: message.mentions.members.first(),
-				"firstUserMention.tag": message.mentions.users.first().tag,
-				"firstUserMention.username": message.mentions.users.first().username,
-				"firstUserMention.avatar": message.mentions.users.first().avatarURL(),
-				author: message.author,
-				"author.tag": message.author.tag,
-				"author.username": message.author.username,
-				"author.avatar": message.author.avatarURL(),
-			});
+			if (!message.mentions.members.first()) {
+				return replace(reply, {
+					author: message.author,
+					"author.tag": message.author.tag,
+					"author.username": message.author.username,
+					"author.avatar": message.author.avatarURL(),
+				});
+			} else {
+				return replace(reply, {
+					firstUserMention: message.mentions.members.first(),
+					"firstUserMention.tag": message.mentions.users.first().tag,
+					"firstUserMention.username": message.mentions.users.first().username,
+					"firstUserMention.avatar": message.mentions.users.first().avatarURL(),
+					author: message.author,
+					"author.tag": message.author.tag,
+					"author.username": message.author.username,
+					"author.avatar": message.author.avatarURL(),
+				});
+			}
 		} else {
 			return reply;
 		}
