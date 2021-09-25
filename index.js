998fd5f41d7e15819fd46e970adc54c3b089cc12
cojored <heppconrad2@gmail.com>
2021-09-24 20:32:13 -0400

    Add JSDoc Data and Update to DJS V13

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [] });
 const CommandsClass = require("./modules/commandsClass.js");
 
 class Bot {
-	constructor(token = undefined, prefix = "?", muterole) {
-		if (token === undefined) {
-			throw "No Token Specified";
-		} else {
-			try {
-				client.login(token);
-			} catch {
-				throw "Problem Logging in to Bot. Check your Token";
-			}
-			this.prefix = prefix;
-			client.on("ready", () => {
-				console.log("Logged in as " + client.user.tag);
-				console.log('Prefix set to "' + this.prefix + '"');
-			});
-		}
-		this.commands = new CommandsClass(prefix, muterole);
-	}
-	initiate() {
-		client.on("message", (message) => {
-			if (message.channel.type === `dm`) return;
-			if (message.author.bot) return;
-			if (!message.content.startsWith(this.prefix)) return;
+    /**
+     * @param {string} token What is the bots token
+     * @param {string} prefix What Should The prefix be
+     * @param {string|number} muterole The muterole id
+     */
+    constructor(token = undefined, prefix = "?", muterole) {
+        if (token === undefined) {
+            throw "No Token Specified";
+        } else {
+            try {
+                client.login(token);
+            } catch {
+                throw "Problem Logging in to Bot. Check your Token";
+            }
+            this.prefix = prefix;
+            client.on("ready", () => {
+                console.log("Logged in as " + client.user.tag);
+                console.log('Prefix set to "' + this.prefix + '"');
+            });
+        }
+        this.commands = new CommandsClass(prefix, muterole);
+    }
 
-			const args = message.content.slice(this.prefix.length).trim().split(/ +/);
-			const command = args.shift();
-			if (this.commands.exists(command)) {
-				this.commands.execute(command, message);
-			}
-		});
-	}
-	setStatus(data) {
-		if (!data.text || !data.type)
-			throw "Status Data Must Include text and type";
-		setTimeout(() => {
-			client.user
-				.setActivity(data.text, {type: data.type.toUpperCase()})
-				.catch(console.error);
-			console.log("Set status to " + data.type + " " + data.text);
-		}, 3000); // Make sure bot is ready before setting status
-	}
+    initiate() {
+        client.on("message", (message) => {
+            if (message.channel.type === `dm`) return;
+            if (message.author.bot) return;
+            if (!message.content.startsWith(this.prefix)) return;
+
+            const args = message.content.slice(this.prefix.length).trim().split(/ +/);
+            const command = args.shift();
+            if (this.commands.exists(command)) {
+                this.commands.execute(command, message);
+            }
+        });
+    }
+
+    /**
+     * @param {Object} statusData
+     * @param {string} statusData.text What Should The Text Be
+     * @param {string} statusData.type What kind of status watching, listening, playing
+     */
+    setStatus(statusData) {
+        if (!statusData.text || !statusData.type)
+            throw "Status Data Must Include text and type";
+        setTimeout(() => {
+            client.user
+                .setActivity(statusData.text, {type: statusData.type.toUpperCase()});
+            console.log("Set status to " + statusData.type + " " + statusData.text);
+        }, 3000); // Make sure bot is ready before setting status
+    }
 }
 
 module.exports = {Bot};
--- modules/commandsClass.js
+++ modules/commandsClass.js
@@ -4,55 +4,74 @@ const formatClass = require("./formatClass");
 const commandData = {};
 
 class CommandsClass {
-	constructor(prefix, muterole) {
-		this.format = new formatClass(prefix);
-		this.actions = new actionClass(this.format);
-		this.muterole = muterole;
-	}
-	create(cmdname, data) {
-		commandData[cmdname] = data;
-		console.log("Loaded Command " + cmdname);
-	}
-	exists(cmdName) {
-		return commandData[cmdName] != null;
-	}
-	get(cmdName) {
-		return commandData[cmdName];
-	}
-	execute(cmdName, message) {
-		let cmd = commandData[cmdName];
-		if (!cmd.action) {
-			if (cmd.reply.text) {
-				message.channel.send(
-					this.format.formatText(cmd.reply.text, message, cmd)
-				);
-			} else if (cmd.reply.embed) {
-				message.channel.send({
-					embed: this.format.formatEmbed(cmd.reply.embed, message, cmd),
-				});
-			}
-		} else {
-			switch (Object.keys(cmd.action)[0].toString()) {
-				case "kick":
-					this.actions.kick(cmd, message);
-					break;
-				case "ban":
-					this.actions.ban(cmd, message);
-					break;
-				case "mute":
-					this.actions.mute(cmd, message, this.muterole);
-					break;
-				case "unmute":
-					this.actions.unmute(cmd, message, this.muterole);
-					break;
-				case "purge":
-					this.actions.purge(cmd, message);
-					break;
-				default:
-					throw "What action should happen\n\nOptions:\nkick\nban\nmute\nunmute\npurge";
-			}
-		}
-	}
+    constructor(prefix, muterole) {
+        this.format = new formatClass(prefix);
+        this.actions = new actionClass(this.format);
+        this.muterole = muterole.toString();
+    }
+
+    /**
+     * @param {string} cmdname Command Name
+     * @param {Object} data
+     * @param {Object} data.reply
+     * @param {Object} [data.reply.action]
+     * @param {string} [data.reply.action.kick] Who are we Kicking
+     * @param {string} [data.reply.action.ban] Who are we Banning
+     * @param {string} [data.reply.action.mute] Who are we Muting
+     * @param {string} [data.reply.action.unmute] Who are we Unmuting
+     * @param {string} [data.reply.action.purge] How many messages are we purging (1 to 100)
+     * @param {Object.<string, string|Object.<string, string>>} data.reply.embed DiscordJS Embed Field Data
+     * @param {string} data.reply.text What should the bot reply with (Text Reply)
+     * @param {string} [data.noMention] What should the bot say when a mention is required but no mention is given (Not Required)
+     * @param {string} [data.noPerms] What should the bot say when permissions are required but the user does not have them (Not Required)
+     */
+    create(cmdname, data) {
+        commandData[cmdname] = data;
+        console.log("Loaded Command " + cmdname);
+    }
+
+    exists(cmdName) {
+        return commandData[cmdName] != null;
+    }
+
+    get(cmdName) {
+        return commandData[cmdName];
+    }
+
+    execute(cmdName, message) {
+        let cmd = commandData[cmdName];
+        if (!cmd.action) {
+            if (cmd.reply.text) {
+                message.channel.send(
+                    this.format.formatText(cmd.reply.text, message, cmd)
+                );
+            } else if (cmd.reply.embed) {
+                message.channel.send({
+                    embed: this.format.formatEmbed(cmd.reply.embed, message, cmd),
+                });
+            }
+        } else {
+            switch (Object.keys(cmd.action)[0].toString()) {
+                case "kick":
+                    this.actions.kick(cmd, message);
+                    break;
+                case "ban":
+                    this.actions.ban(cmd, message);
+                    break;
+                case "mute":
+                    this.actions.mute(cmd, message, this.muterole);
+                    break;
+                case "unmute":
+                    this.actions.unmute(cmd, message, this.muterole);
+                    break;
+                case "purge":
+                    this.actions.purge(cmd, message);
+                    break;
+                default:
+                    throw "What action should happen\n\nOptions:\nkick\nban\nmute\nunmute\npurge";
+            }
+        }
+    }
 }
 
 module.exports = CommandsClass;
